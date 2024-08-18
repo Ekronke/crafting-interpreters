3b8a61a6505006675f7fa37ae49a9c27e19a93bc
ekronke <ekronke@kth.se>
2024-08-18 14:23:22 +0200

    Mabye I will complete this book during winter break

--- c/helloworld.c
+++ c/helloworld.c
@@ -1,17 +1,34 @@
 // Double linked list heap allocated stirngs
 // Functions for insert, find, delete
+// Mabye not ready yet and scool starts soon
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node {
+struct node {
 	int data;
-	struct Node* prev;
-	struct Node* next;
+	struct node* prev;
+	struct node* next;
 };
 
-struct Node *createNode(int new_data) {
-	struct Node *new_node = (struct Node *)
-	malloc(sizeof(struct Node));
+struct node* head = NULL;
+struct node* tail = NULL;
+
+struct node *create_node(int new_data) {
+	struct node* new_node = (struct node*)malloc(sizeof(struct node));
 	new_node->data = new_data;
 	new_node->next = NULL;
 	new_node->prev = NULL;
 	return new_node;
+}
+
+void inster_at_head(int new_data) {
+	struct node* new_node = create_node(new_data);
+	if (head == NULL) {
+		head = new_node;
+		tail = new_node;
+	} else {
+		new_node->next = head;
+		head->prev = new_node;
+		head = new_node;
+	}
 }
