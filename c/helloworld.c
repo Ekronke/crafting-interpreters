c0ffe7a33c6f7053f0b6eeebbcd67795db3d147e
ekronke <ekronke@kth.se>
2024-08-18 13:43:58 +0200

    First challanges

---
+++ c/helloworld.c
@@ -0,0 +1,17 @@
+// Double linked list heap allocated stirngs
+// Functions for insert, find, delete
+
+struct Node {
+	int data;
+	struct Node* prev;
+	struct Node* next;
+};
+
+struct Node *createNode(int new_data) {
+	struct Node *new_node = (struct Node *)
+	malloc(sizeof(struct Node));
+	new_node->data = new_data;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return new_node;
+}
